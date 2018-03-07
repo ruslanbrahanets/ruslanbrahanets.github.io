833eb7a933cfea2f6156c3c0d055445f7f6bc98e
ruslanbrahanets <ruslan.brahanets@gmail.com>
2018-03-07 21:07:55 +0200

    Brahanets Ruslan
    Personal portfolio web site

--- Vistex/js/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(window).on('load', function () {
-      var $preloader = $('#page'),
-            $svg_anm   = $('#loader');
-        $svg_anm.delay(1000).fadeOut('slow');
-        $preloader.delay(1000).fadeOut('slow');
-    });
-});
